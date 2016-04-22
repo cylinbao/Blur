3686a380c96d1a979e851082d0115697759dbef3
Chien Yu Lin <myislin@gmail.com>
2016-04-22 13:53:27 +0800

    add some images, modify computing weights

--- blur.c
+++ blur.c
@@ -1,8 +1,8 @@
 #include "bmp.h"
 
 #define kern_size 5
+#define max_weight 273
 
-int weight = 273;
 int gaussian_filter[5][5] = {
 	{1, 4, 7, 4, 1},
 	{4, 16, 26, 16, 4},
@@ -13,15 +13,15 @@ int gaussian_filter[5][5] = {
 
 void blur(BMP *bmp, BMP *blurbmp)
 {
-	int i, j, k, l, idx1, idx2;
+	int i, j, k, l, idx1, idx2, weight;
 	int acc[3];
 	int kern_len = kern_size/2;
-	//printf("kern_len = %d\n", kern_len);
 	Pixel *pixel;
 
 	for(i = 0; i < bmp->height; i++){
 		for(j = 0; j < bmp->width; j++){
 			acc[0] = 0; acc[1] = 0; acc[2] = 0;
+			weight = max_weight;
       for(k = 0; k < kern_size; k++){                                          
         for(l = 0; l < kern_size; l++){                                        
           idx1 = j - kern_len + l;                                             
@@ -33,6 +33,8 @@ void blur(BMP *bmp, BMP *blurbmp)
 						acc[1] += gaussian_filter[k][l] * (pixel->G - 0);
 						acc[2] += gaussian_filter[k][l] * (pixel->B - 0);
 					}
+					else
+						weight -= gaussian_filter[k][l];
         }                                                                        
       }		
 			pixel = (Pixel *) &blurbmp->data[(i*blurbmp->width + j)*sizeof(Pixel)];
@@ -50,10 +52,8 @@ int main(int argc, char *argv[]) {
 		BMP bmp, blurbmp;
 
     bmpLoad(&bmp, input);
-    //bmpPrint(&bmp);
 		blurbmp = bmp;
 		blur(&bmp, &blurbmp);
-    //bmpPrint(&blurbmp);
     bmpSave(&blurbmp, output);
 
     return 0;
